261c72bf2bf5647e0907e5411fa8f5ae67f0bd6c
Edwin <edwin@wonderbly.com>
2019-10-01 14:18:18 +0100

    improvement: Update example app to showcase features of nav

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { withNav, withNavItem } from 'react-skeletal-nav';
 
 export const Nav = withNav(
@@ -8,7 +9,7 @@ export const Nav = withNav(
       // Hide if not visible, but don't unmount so nested navs stay mounted
       <div style={{ display: isVisible ? 'block' : 'none' }}>
         {/* Never render the back button on the "root" nav */}
-        {route !== 'root' && <button onClick={goBack}>Back</button>}
+        {route !== 'root' && <button className="go-back" onClick={goBack}>{'<'} Back</button>}
 
         {/* Render children, including nested navs */}
         {children}
@@ -16,25 +17,52 @@ export const Nav = withNav(
     )
 );
 
-export const NavItem = withNavItem(({ children, href, onClick, title }) => (
-  <div>
-    {href && <a href="">{title}</a>}
-    {!href && <button onClick={onClick}>{title}</button>}
+export const NavItem = withNavItem(({ children, href, onClick, title, setActiveRoute, isActive }) => {
+  const [isLinkActive, setIsLinkActive] = useState(false);
+  useEffect(() => {
+    if (href === window.location.pathname) {
+      setIsLinkActive(true);
+      setActiveRoute();
+    }
+  }, []);
+  return (
+    <div className={`nav-item${isActive || isLinkActive ? ' active' : ''}`}>
+      {href && <a href={href}>{title}</a>}
+      {!href && <button onClick={onClick}>{title} {'>'}</button>}
+      {/* Render nested navs */}
+      {children}
+    </div>
+  );
+});
 
-    {/* Render nested navs */}
-    {children}
-  </div>
-));
+const page = title => () => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
 
 export default () => (
-  <Nav>
-    <NavItem title="Item 1" href="/" />
-    <NavItem title="Item 2" href="/" />
-    <NavItem title="Sub Nav">
+  <Router>
+    <header>
+      <h1>React Skeletal Nav</h1>
       <Nav>
-        <NavItem title="Subitem 1" href="/" />
-        <NavItem title="Subitem 2" href="/" />
+        <NavItem title="Home" href="/" />
+        <NavItem title="Personal">
+          <Nav>
+            <NavItem title="About Me" href="/about" />
+            <NavItem title="Code" href="/code" />
+          </Nav>
+        </NavItem>
+        <NavItem title="Contact" href="/contact" />
       </Nav>
-    </NavItem>
-  </Nav>
+    </header>
+    <main>
+      <Route exact path="/" render={page("Home")} />
+      <Route exact path="/about" render={page("About")} />
+      <Route exact path="/code" render={page("Code")} />
+      <Route exact path="/contact" render={page("Contact")} />
+    </main>
+  </Router>
 );
